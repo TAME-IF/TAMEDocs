970b4afce2248cce22a4f44d78b8a77670ef01d0
mtrop <mtropiano@gmail.com>
2017-05-09 18:35:29 -0400

    Tweaked TS language for Prism

--- site-assets/js/prism-tamescript.js
+++ site-assets/js/prism-tamescript.js
@@ -20,7 +20,7 @@ Prism.languages.tamescript = {
 	},
 	'entry-name': /\b(INIT|AFTERREQUEST|START|ON(ACTION(WITH(OTHER)?)?|MODALACTION|WORLDBROWSE|ROOMBROWSE|PLAYERBROWSE|CONTAINERBROWSE|AMBIGUOUSACTION|BADACTION|INCOMPLETEACTION|UNKNOWNACTION|FAILEDACTION|FORBIDDENACTION|ROOMFORBIDDENACTION))\b/i,
 	'function': /\b(NOOP|QUEUE(ACTION(FOR(TAGGEDOBJECTSIN|OBJECTSIN)?|STRING|OBJECT(2)?)?)|ADDCUE|TEXT(F(LN)?|LN)|PAUSE|WAIT|TIP|INFO|AS(BOOLEAN|INT|FLOAT|STRING)|STR(LENGTH|CONCAT|REPLACE(PATTERN(ALL)?)?|INDEX|LASTINDEX|CONTAINS(PATTERN|TOKEN)?|STARTSWITH|ENDSWITH|LOWER|UPPER|CHAR|TRIM)|SUBSTRING|FLOOR|CEILING|ROUND|FIX|SQRT|PI|E|SIN|COS|TAN|MIN|MAX|CLAMP|RANDOM|FRANDOM|GRANDOM|TIME|SECONDS|MINUTES|HOURS|DAYS|FORMATTIME|OBJECT(HAS(NAME|TAG|NOOWNER)?|COUNT)|ADDOBJECT(NAME|TAG(TOALLIN)?)|REMOVEOBJECT(NAME|TAG(FROMALLIN)?)?|GIVEOBJECT|MOVEOBJECTSWITHTAG|HASOBJECT|PLAYER(ISINROOM|CANACCESSOBJECT)|BROWSE(TAGGED)?|SET(PLAYER|ROOM)|PUSHROOM|POPROOM|SWAPROOM|CURRENT(PLAYERIS|ROOMIS)|NOCURRENT(PLAYER|ROOM)|IDENTITY)/i,
-	'keyword': /\b(if|else|while|for|quit|end|break|continue|module|world|room|player|object|container|action|general|modal|transitive|ditransitive|open|restricted|named|tagged|modes|uses|conjunctions|determiners|forbids|allows|local|clear|archetype|function|return)\b/i,
+	'keyword': /\b(if|else|while|for|quit|end|break|continue|module|world|room|player|object|container|action|general|modal|transitive|ditransitive|open|restricted|named|tagged|modes|uses|conjunctions|determiners|forbids|allows|local|clear|archetype|function|return|this|override|extend)\b/i,
 	'boolean': /\b(true|false|Infinity|NaN)\b/i,
 	'number': /\b-?(?:0x[\da-f]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
 	'operator': /(\(|\)|\[|\]|:|;|\.|,|\+|\-|\!|~|\*\*?|\/|%|&\||\^|<<?|<=?|>>?>?|>=?|==?=?|\!==?)/
